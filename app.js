ecefc88184f609732c148fd0dee42e0f65102f92
Daksh <Dakshjkhadalpur177@gmail.com>
2025-03-12 14:33:55 +0530

    fixed vey much set database to global

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(flash());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"views")));
 app.set("view engine","ejs");
 
 app.use("/owners",ownerRouter);
